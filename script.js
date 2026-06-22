2026ee919a61752c8624196a759bb768a5b36f88
0D3viLEyeD0 <159899841+0D3viLEyeD0@users.noreply.github.com>
2026-06-22 10:55:33 +0200

    igen

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ const skillElemet = document.querySelectorAll('.skills-list li');
 
 skillElemet.forEach(function(skill) {
     skill.addEventListener('click', function() {
-        
         this.classList.toggle('active');
     });
 });
+
+const textElement = document.getElementById('typing-text');
+const text = "Kezdő szoftverfejlesztő portfóliója...";
+let index = 0;
+
+function typeWriter() {
+    if (textElement && index < text.length) {
+        textElement.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeWriter, 100);
+    }
+}
+
+window.onload = typeWriter;
